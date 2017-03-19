7e6a036a6ee846433dee1595113581e42f333270
郝旭东 <haoxd@njhhet.com>
2017-03-19 22:05:21 +0800

    新增mybatis xml文件 sql查询入参规范（paramData）

---
+++ src/main/java/com/tb/common/org/gtmd/frame/tools/paramData.java
@@ -0,0 +1,25 @@
+package com.tb.common.org.gtmd.frame.tools;
+
+import java.util.HashMap;
+
+/**
+ * @author acer11 作者：haoxud
+ * @param <String>
+ * @param <Object>
+ *            创建时间：2017年3月19日 下午9:50:22 项目名称：tb-common
+ * @author daniel
+ * @version 1.0
+ * @since JDK 1.6.0_21 文件名称：paramData.java 类说明：拓展map工具,自定义
+ */
+public class paramData<String, Object> extends HashMap {
+	
+	
+	public paramData() {
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
